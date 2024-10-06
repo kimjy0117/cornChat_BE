ebccdc00d29bd78e7241ae678a5d5d7504cb0baa
Kim <kimjy2000117@gmail.com>
2024-10-06 10:52:01 +0900

    🎉 프로젝트 초기설정
    프로젝트 초기설정

---
+++ src/main/java/org/example/cornchat_be/CornChatBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.cornchat_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CornChatBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CornChatBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/cornchat_be/CornChatBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cornchat_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CornChatBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
