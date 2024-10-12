5cbd05192958484064cbc1d9afa216d80c4c6497
Kim <kimjy2000117@gmail.com>
2024-10-12 19:22:18 +0900

    ✨ Jwt 로직 구현
    로그인 시 access토큰, refresh토큰 발급 / 로그아웃 시 refresh토큰 DB에서 제거

---
+++ src/main/java/org/example/cornchat_be/jwt/LoginFilter.java
@@ -0,0 +1,115 @@
+package org.example.cornchat_be.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.example.cornchat_be.jwt.entity.RefreshEntity;
+import org.example.cornchat_be.jwt.repository.RefreshRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.Iterator;
+
+public class LoginFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+    private final JWTUtil jwtUtil;
+    private final RefreshRepository refreshRepository;
+
+    public LoginFilter(AuthenticationManager authenticationManager, JWTUtil jwtUtil, RefreshRepository refreshRepository) {
+        this.authenticationManager = authenticationManager;
+        this.jwtUtil = jwtUtil;
+        this.refreshRepository = refreshRepository;
+
+        // 여기에서 로그인 처리할 URL 설정
+        setFilterProcessesUrl("/login"); // 이 URL로 POST 요청이 들어오면 필터가 동작
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+
+        // 로그 추가
+        System.out.println("Attempting authentication for user: " + request.getParameter("email"));
+
+
+        //클라이언트 요청에서 email, password 추출
+        String email = obtainEmail(request);
+        String password = obtainPassword(request);
+
+        // Spring Security에서 email과 password를 검증하기 위해서는 token에 담아야 함
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(email, password, null);
+
+        // token에 담은 검증을 위한 AuthenticationManager로 전달
+        // 여기서 검증에 성공하면 successfulAuthentication 메서드 호출
+        return authenticationManager.authenticate(authToken);
+    }
+
+    //이메일을 요청해서 추출하는 메서드 추가
+    protected String obtainEmail(HttpServletRequest request) {
+        return request.getParameter("email");
+    }
+
+    //로그인 성공시 실행하는 메소드(여기서 jwt토큰을 발행)
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication){
+        //유저 email 추출
+        String email = authentication.getName();
+        System.out.println("email: " + email);
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
+        GrantedAuthority auth = iterator.next();
+        String role = auth.getAuthority();
+
+        //토큰 생성
+        String access = jwtUtil.createJwt("access", email, role, 600000L);
+        String refresh = jwtUtil.createJwt("refresh", email, role, 86400000L);
+
+        //Refresh 토큰 저장
+        addRefreshEntity(email, refresh, 86400000L);
+
+        //응답 설정
+        response.setHeader("access", access);
+        response.addCookie(createCookie("refresh", refresh));
+        response.setStatus(HttpStatus.OK.value());
+    }
+
+    //로그인 실패시 실행하는 메소드
+    @Override
+    protected  void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) {
+        response.setStatus(401);
+    }
+
+    private void addRefreshEntity(String email, String refresh, Long expiredMs) {
+
+        Date date = new Date(System.currentTimeMillis() + expiredMs);
+
+        RefreshEntity refreshEntity = new RefreshEntity();
+        refreshEntity.setEmail(email);
+        refreshEntity.setRefresh(refresh);
+        refreshEntity.setExpiration(date.toString());
+
+        refreshRepository.save(refreshEntity);
+    }
+
+    private Cookie createCookie(String key, String value) {
+
+        Cookie cookie = new Cookie(key, value);
+        cookie.setMaxAge(24*60*60);
+        //https로 할 경우 아래에 주석을 풀어주어야 함
+        //cookie.setSecure(true);
+        //cookie.setPath("/");
+        cookie.setHttpOnly(true);
+
+        return cookie;
+    }
+
+}
---
+++ src/main/java/org/example/cornchat_be/jwt/service/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package org.example.cornchat_be.jwt.service;
+
+import org.example.cornchat_be.domain.user.entity.User;
+import org.example.cornchat_be.domain.user.repository.UserRepository;
+import org.example.cornchat_be.jwt.dto.CustomUserDetails;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+
+        if (user != null){
+            return new CustomUserDetails(user);
+        }
+        return null;
+    }
+
+}
