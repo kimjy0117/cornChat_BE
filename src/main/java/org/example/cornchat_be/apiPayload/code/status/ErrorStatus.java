145cb374cee4cc623a32c6e81639eddadbc21b27
Kim <kimjy2000117@gmail.com>
2024-11-24 04:04:11 +0900

    🔨 성공 및 에러코드 enum파일 추가
    ErrorStatus 및 SuccessStatus 코드 추가

--- src/main/java/org/example/cornchat_be/apiPayload/code/status/ErrorStatus.java
+++ src/main/java/org/example/cornchat_be/apiPayload/code/status/ErrorStatus.java
@@ -15,8 +15,24 @@ public enum ErrorStatus {
     _FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다."),
     _CONFLICT(HttpStatus.CONFLICT,"COMMON409","잘못된 입력입니다."),
 
-    // password 에러
-    _ALREADY_PASSWORD_SAME(HttpStatus.BAD_REQUEST, "PASSWORD4001", "기존 비밀번호와 동일합니다.")
+    // 사용자 정보
+    _ALREADY_EXIST_EMAIL(HttpStatus.CONFLICT, "EXISTEMAIL409", "이미 사용중인 이메일입니다."),
+    _ALREADY_NOT_EXIST_EMAIL(HttpStatus.BAD_REQUEST, "NOTEXISTEMAIL400", "존재하지 않는 이메일 주소입니다."),
+    _ALREADY_EXIST_USERID(HttpStatus.CONFLICT, "USERID409", "이미 사용중인 아이디입니다."),
+    _ALREADY_EXIST_NUMBER(HttpStatus.CONFLICT, "PHONE409", "이미 사용중인 번호입니다."),
+    _ALREADY_EXIST_PASSWORD(HttpStatus.CONFLICT, "PASSWORD409", "기존 비밀번호와 일치합니다. 새로운 비밀번호를 입력해주세요."),
+    _NOT_EXIST_USER(HttpStatus.BAD_REQUEST, "NOTEXISTUSER400", "존재하지 않는 사용자입니다."),
+
+    // 친구
+    _DUPLICATED_FRIEND(HttpStatus.CONFLICT, "DUPFRIEND409", "이미 등록된 친구입니다."),
+    _ADD_FRIEND_MYSELF(HttpStatus.BAD_REQUEST, "ADDMYSELF404", "자신은 친구로 추가할 수 없습니다."),
+
+    //이메일 인증
+    _VERIFY_CODE_FAIL(HttpStatus.BAD_REQUEST, "VERIFYCODE400", "인증번호가 일치하지 않습니다."),
+
+    //채팅
+    _NOT_EXIST_ROOM_ID(HttpStatus.BAD_REQUEST, "NOTEXISTROOMID400", "존재하지 않는 채팅방입니다."),
+    _ALREADY_EXIST_FRIEND(HttpStatus.CONFLICT, "EXISTFRINED409", "이미 초대된 친구입니다."),
     ;
 
     private final HttpStatus httpStatus;
@@ -28,7 +44,6 @@ public enum ErrorStatus {
                 .status(httpStatus)
                 .errorCode(code)
                 .message(message)
-                .isSuccess(false)
                 .build();
     }
 }
--- src/main/java/org/example/cornchat_be/apiPayload/code/status/SuccessStatus.java
+++ src/main/java/org/example/cornchat_be/apiPayload/code/status/SuccessStatus.java
@@ -10,9 +10,39 @@ import org.springframework.http.HttpStatus;
 public enum SuccessStatus {
     // 일반적인 응답
     _GET_OK(HttpStatus.OK, "COMMON200", "200 OK"),
-    _POST_OK(HttpStatus.CREATED,"COMMON201","201 Created."),
+    _POST_OK(HttpStatus.OK, "COMMON200", "200 OK"),
+    _CREATE_OK(HttpStatus.CREATED,"COMMON201","201 Created."),
     _DELETE_OK(HttpStatus.ACCEPTED,"COMMON202","삭제되었습니다."),
-    _UPDATE_SUCCESS(HttpStatus.NO_CONTENT, "COMMON204", "업데이트 완료되었습니다.");
+    _PATCH_OK(HttpStatus.NO_CONTENT, "COMMON204", "업데이트 완료되었습니다."),
+
+    // 유저
+    _CHECK_USERID_DUP_TRUE(HttpStatus.OK, "IDCHECKTRUE200", "중복되는 아이디입니다."),
+    _CHECK_USERID_DUP_FALSE(HttpStatus.OK, "IDCHECKFALSE200", "사용할 수 있는 아이디입니다."),
+    _CHECK_PHONENUM_DUP_TRUE(HttpStatus.OK, "PHONECHECKTRUE200", "중복되는 전화번호입니다."),
+    _CHECK_PHONENUM_DUP_FALSE(HttpStatus.OK, "PHONECHECKFALSE200", "사용할 수 있는 전화번호입니다."),
+    _JOIN_SUCCESS(HttpStatus.OK, "JOIN201", "회원가입이 성공하였습니다."),
+    _FIND_PASSWORD_SUCCESS(HttpStatus.NO_CONTENT, "PASSWORD204", "비밀번호 변경되었습니다."),
+    _SET_USERNAME_SUCCESS(HttpStatus.NO_CONTENT, "SETUSERNAME204", "이름이 변경되었습니다."),
+    _SET_STATUSMESSAGE_SUCCESS(HttpStatus.NO_CONTENT, "SETSTATUSMESSAGE204", "상태메시지가 변경되었습니다."),
+    _SEND_CODE_SUCCESS(HttpStatus.CREATED, "SENDCODE201", "인증번호가 전송되었습니다."),
+    _VERIFY_CODE_SUCCESS(HttpStatus.OK, "VERIFYCODE200", "이메일 인증이 성공하였습니다."),
+    _DELETE_USER_SUCCESS(HttpStatus.ACCEPTED, "DLETEUSER202", "회원 탈퇴 되었습니다."),
+
+    //친구
+    _ADD_FRIEND_SUCCESS(HttpStatus.OK, "ADDFRIEND200", "친구 등록에 성공하였습니다."),
+    _FIND_FRIEND_LIST_SUCCESS(HttpStatus.OK, "FINDFRIEND200", "친구 목록 가져오기에 성공하였습니다."),
+    _SET_FRIENDNAME_SUCCESS(HttpStatus.NO_CONTENT, "SETFRIENDNAME204", "친구 이름이 변경되었습니다."),
+    _DELETE_FRIEND_SUCCESS(HttpStatus.ACCEPTED, "DELETEFRIEND202", "해당 친구가 삭제되었습니다."),
+
+    //채팅
+    _CREATE_CHATROOM_SUCCESS(HttpStatus.CREATED, "CREATECHATROOM204", "채팅방이 생성되었습니다."),
+    _GET_CHATROOM_DETAILS_SUCCESS(HttpStatus.OK, "GETCHATROOMDETAILS200", "채팅방 정보를 가져왔습니다."),
+    _GET_CHAT_HISTORY_EMPTY_SUCCESS(HttpStatus.OK, "GETCHATEMPTY200", "채팅방 기록이 비어있습니다."),
+    _GET_CHAT_HISTORY_SUCCESS(HttpStatus.OK, "GETCHAT200", "채팅방 기록을 성공적으로 가져왔습니다."),
+    _GET_CHATROOM_LIST_SUCCESS(HttpStatus.OK, "GETCHATROOMLIST200", "채팅방 리스트를 성공적으로 가져왔습니다."),
+    _LEAVE_CHATROOM_SUCCESS(HttpStatus.ACCEPTED, "LEAVECHATROOM202", "채팅방 나가기에 성공하였습니다."),
+    _ADD_FRIEND_CHATROOM_SUCCESS(HttpStatus.OK, "ADDFRINEDCHATROOM200", "채팅방 초대에 성공하였습니다."),
+    ;
 
     private final HttpStatus httpStatus;
     private final String code;
@@ -20,10 +50,18 @@ public enum SuccessStatus {
 
     public <T> SuccessDto<T> convertSuccessDto(T data) {
         return SuccessDto.<T>builder()
-                .isSuccess(true)
                 .successCode(code)
                 .message(message)
                 .data(data)
+                .httpStatus(httpStatus)
+                .build();
+    }
+
+    public <T> SuccessDto<T> convertSuccessDto() {
+        return SuccessDto.<T>builder()
+                .successCode(code)
+                .message(message)
+                .data(null)
                 .httpStatus(httpStatus)
                 .build();
     }
